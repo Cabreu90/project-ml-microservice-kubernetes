68722a99c7c0f702b4047945fc551563ba916464
Cesarin <abreu_cesarin@yahoo.com>
2020-05-02 12:40:45 -0400

    Feat: Kubernetes

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,16 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath=cabreu90/app
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run app --image=cabreu90/app --port=80
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl expose deployment app --type=LoadBalancer --port=8000 --target-port=80
